9340592caeafab240edf292f746e40f7ce9e2eb3
Kevin Mevada <mevadakevin@gmail.com>
2026-01-10 19:57:48 -0600

    Exception and Logger

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+
+# Create logs directory
+LOGS_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOGS_DIR, exist_ok=True)
+
+# Create log file name
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOGS_DIR, LOG_FILE)
+
+# Configure logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    level=logging.INFO,
+    format='[%(asctime)s] [%(levelname)s] [%(name)s] [%(filename)s:%(lineno)d] - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
